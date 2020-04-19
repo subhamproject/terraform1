1ee6cef247e86c11b83ca0669001243bc23090ac
Subham Mandal <subhammandal01@gmail.com>
2020-04-19 23:11:59 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,9 +1,15 @@
+
+locals {
+  tag_prefix = "Main_${data.aws_caller_identity.myidentity.account_id}_${data.aws_region.myregion.name}"
+}
+
+
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.SUBNET_CIDR
   map_public_ip_on_launch = true
   tags = {
-    Name = "Main"
+    Name = "${local.tag_prefix}_subnet"
   }
 }
 
